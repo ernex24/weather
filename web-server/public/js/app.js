c6519e714a641981ecd89e20808eeaa6c969851c
Ernesto <ernex24@gmail.com>
2019-04-18 18:21:52 +0200

    =initcommit

---
+++ web-server/public/js/app.js
@@ -0,0 +1,34 @@
+console.log('Client side javascript file is loaded!')
+
+
+  const weatherForm = document.querySelector('form')
+  const inputLocation = document.querySelector('input')
+  const messageOne = document.querySelector('#message-1')
+  const messageTwo = document.querySelector('#message-2')
+
+
+ 
+
+  weatherForm.addEventListener('submit', (e) => {
+      e.preventDefault()
+      messageOne.textContent = 'loading'
+       var value = inputLocation.value
+        console.log(value)
+
+        fetch(`http://localhost:3000/weather?address=${value}`)
+  .then(function(response) {
+    return response.json().then((data) => {
+        if (data.error) {
+            messageOne.textContent = 'No se ha cargado una mierda'
+        } else { 
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+            console.log(data.location);
+            console.log(data.forecast);
+        }
+    });
+  })
+  })
+
+  
+  
